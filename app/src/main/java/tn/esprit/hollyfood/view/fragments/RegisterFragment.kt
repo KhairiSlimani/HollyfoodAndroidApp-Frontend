ecbab63284b70ece3146056196015d523bbc76d6
Khairi Slimani <61380055+KhairiSlimani@users.noreply.github.com>
2023-03-21 17:16:01 +0100

    Register Fragment

---
+++ app/src/main/java/tn/esprit/hollyfood/view/fragments/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package tn.esprit.hollyfood.view.fragments
+
+import androidx.fragment.app.Fragment
+import tn.esprit.hollyfood.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
